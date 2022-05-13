2d62477147bd9902dfed0c6f0aaaa69036a1605c
nilomr <nilomerinorecalde@gmail.com>
2022-05-13 17:16:21 +0100

    first commit of slurm test file

---
+++ notebooks/slurm/test.py
@@ -0,0 +1,35 @@
+
+import os
+from pathlib import Path
+import sys
+import time
+import git
+from pykanto.utils.paths import ProjDirs
+from pykanto.parameters import Parameters
+from pykanto.dataset import KantoData
+from pykanto.utils.write import make_tarfile
+import ray
+
+
+DATASET_ID = "GREAT_TIT"
+DATA_PATH = Path('/data/zool-songbird/shil5293/projects/great-tit-song/data')
+PROJECT = Path('/data/zool-songbird/shil5293/projects/greti-main')
+RAW_DATA = DATA_PATH / "segmented" / "GRETI_2021"
+DIRS = ProjDirs(PROJECT, RAW_DATA, mkdir=True)
+print(DIRS)
+
+redis_password = sys.argv[1]
+ray.init(address=os.environ["ip_head"], _redis_password=redis_password)
+print(ray.cluster_resources())
+
+params = Parameters(dereverb=True, verbose=True)
+dataset = KantoData(
+    DATASET_ID,
+    DIRS,
+    parameters=params,
+    overwrite_dataset=True,
+    overwrite_data=True,
+    random_subset=100
+)
+
+print('Done')
