baa3b33113ec1517e5c9561e85663aca041ef682
nilomr <nilomerinorecalde@gmail.com>
2022-07-18 10:33:32 +0100

    first commit - infer song community structure

---
+++ notebooks/community-detection.py
@@ -0,0 +1,10 @@
+# https://github.com/taynaud/python-louvain
+# Some code to plot the results: https://doi.org/10.1017/ehs.2022.15
+
+
+# calculate posterior probabilities of membership in communities
+# https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0267501
+# R code https://osf.io/5fter/
+
+# Using identity calls to detect structure in acoustic datasets:
+# https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/2041-210X.13644
