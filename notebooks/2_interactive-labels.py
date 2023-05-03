858a1f49a48888dfed001383df281146a94552ef
nilomr <nilomerinorecalde@gmail.com>
2023-05-03 01:18:55 +0100

    handle larger number of clusters

--- notebooks/2_interactive-labels.py
+++ notebooks/2_interactive-labels.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import git
 import pandas as pd
+from bokeh.palettes import Category20_20, Set3_12
 from pykanto.utils.io import load_dataset
 from pykanto.utils.paths import ProjDirs
 
@@ -42,9 +43,15 @@ using existing labels by default. App user guide in the docs.
 use_app = False
 
 if use_app:
-    dataset.open_label_app()
+    # get number of unique auto_class per ID:
+    nlabs = dataset.data.groupby("ID").auto_class.nunique().max()
+    palette = Set3_12 if nlabs <= 12 else Category20_20
+    dataset.open_label_app(palette=palette)
     dataset = dataset.reload()
 else:
     df = pd.read_csv(DIRS.DATASET.parent / "checked_labels.csv", index_col=0)
-    dataset.data.insert(2, "class_label", df.class_label.astype("string"))
+    try:
+        dataset.data.insert(2, "class_label", df.class_label.astype("string"))
+    except ValueError:
+        dataset.data["class_label"] = df.class_label.astype("string")
     dataset.save_to_disk()
